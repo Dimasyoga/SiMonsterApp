c0da8124a0feeb4649ef134d3da8695f067de947
dimasyoga <dimasyogapra@gmail.com>
2019-12-24 08:33:40 +0700

    add red modal

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableHighlight, Modal } from 'react-native';
 import firebase from 'firebase';
-// import PushNotification from 'react-native-push-notification';
-// import PushController from './PushController'
+import Popup from 'reactjs-popup'
 
 var firebaseConfig = {
   apiKey: "AIzaSyCq89rpk8CsXk_eAy_-WR7Dyo7kai4SdfA",
@@ -24,7 +23,9 @@ export default class App extends Component {
       chartData1:[],
       chartData2:[],
       color1:'#FFFFFF',
-      color2:'#FFFFFF'
+      color2:'#FFFFFF',
+      modal1Visible:false,
+      modal2Visible:false
     }
   }
 
@@ -56,22 +57,44 @@ export default class App extends Component {
     const rootRef = firebase.database().ref();
     // const rootRef = firebase.database();
     rootRef.on('value', snap => {
-      var list1 = Object.values(snap.val()['sm00']['ts'])
-      // var list2 = Object.values(snap.val()['tong 2'])
-      list1.sort((a, b) => (a.time > b.time) ? 1 : -1 )
-      var list2 = list1.filter(this.myFunct)
-      // console.log("Data: ", list2)
-      // console.log("Last state: ", list2.slice(-1)[0]['state'])
-      if(list2.slice(-1)[0]['state'] === 2){
-        alert('SiMonster 1 full')
+      if(snap.val()['sm00']['state'] === 2){
+        this.setModal1Visible(true);
       }
-      this.setState({ 
-        chartData1: list2,
-        color1:this.getColor(list2)
-        // chartData2: list2.sort((a, b) => (a.timestamp > b.timestamp) ? 1 : -1 )
-      }, () => {
-        console.log("getData success")
-      });
+      if(snap.val()['sm01']['state'] === 2){
+        this.setModal2Visible(true);
+      }
+      var list1 = Object.values(snap.val()['sm00']['ts'])
+      var list2 = Object.values(snap.val()['sm01']['ts'])
+      // console.log("Data: ", list1)
+      list1.sort((a, b) => (a.time > b.time) ? 1 : -1 )
+      list2.sort((a, b) => (a.time > b.time) ? 1 : -1 )
+      // console.log("Last state: ", list1)
+      list1 = list1.filter(this.myFunct)
+      list2 = list2.filter(this.myFunct)
+      
+      if(typeof list1 != "undefined" && list1 != null && list1.length != null
+      && list1.length > 0){
+        this.setState({ 
+          chartData1: list1,
+          color1:this.getColor(list1),
+        }, () => {
+          console.log("getData1 success")
+        });
+      }else{
+        console.log("SiMonster 1 no data")
+      }
+
+      if(typeof list2 != "undefined" && list2 != null && list2.length != null
+      && list2.length > 0){
+        this.setState({ 
+          chartData2: list2,
+          color2:this.getColor(list2),
+        }, () => {
+          console.log("getData2 success")
+        });
+      }else{
+        console.log("SiMonster 2 no data")
+      }
     });
   }
 
@@ -79,18 +102,16 @@ export default class App extends Component {
     this.getData(theChosenOne);
   }
 
-  // sendNotification() {
-  //   PushNotification.localNotification({
-  //     message: 'state 2 capacity',
-  //     importance: "high",
-  //     priority: "high",
-  //     vibrate: true,
-  //   });
-  // }
-
   _onPressButton(){
     alert('you tapped the button')
-    // this.sendNotification
+  }
+
+  setModal1Visible(visible) {
+    this.setState({modal1Visible: visible});
+  }
+
+  setModal2Visible(visible) {
+    this.setState({modal2Visible: visible});
   }
 
   render() {
@@ -113,7 +134,45 @@ export default class App extends Component {
             </TouchableHighlight>
           </View>
         </View>
-        {/* <PushController /> */}
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modal1Visible}
+          onRequestClose={() => {
+            this.setModal1Visible(!this.state.modal1Visible);
+          }}>
+          <View style={styles.screen}>
+            <View style={styles.mod}>
+              <Text style={styles.modalText}>SiMonster 1 Full!</Text>
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModal1Visible(!this.state.modal1Visible);
+                }}>
+                <Text style={styles.buttonText}>Close</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modal2Visible}
+          onRequestClose={() => {
+            this.setModal2Visible(!this.state.modal2Visible);
+          }}>
+          <View style={styles.screen}>
+            <View style={styles.mod}>
+              <Text style={styles.modalText}>SiMonster 2 Full!</Text>
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModal2Visible(!this.state.modal2Visible);
+                }}>
+                <Text style={styles.buttonText}>Close</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -132,6 +191,18 @@ const styles = StyleSheet.create({
     margin: 10,
     color:'#ffffff'
   },
+  mod: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor:'#fc0000',
+  },
+  modalText: {
+    fontSize: 72,
+    textAlign: 'center',
+    margin: 10,
+    color:'#000000'
+  },
   header: {
     flex: 1,
     justifyContent: 'center',
@@ -147,7 +218,6 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     width: 260,
     alignItems: 'center',
-    // backgroundColor: '#2196F3'
   },
   buttonText: {
     textAlign: 'center',
